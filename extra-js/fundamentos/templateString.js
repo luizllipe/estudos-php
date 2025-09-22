8daf8fb97a37a70603900474f3a1ff4fd4eba56a
luizllipe <luisecaduandrade@gmail.com>
2025-09-22 19:32:58 -0300

    $343.Exemplos Básicos de Funções

--- extra-js/fundamentos/templateString.js
+++ extra-js/fundamentos/templateString.js
@@ -8,5 +8,5 @@ console.log(concatenacao, template)
 // expressoes...
 console.log(`1 + 1 = ${1 + 1}`)
 
-const up = texto => texto.toUpperCase()
+const up = texto => texto  .toUpperCase()
 console.log(`Ei... ${up(`cuidado`)}!`)
